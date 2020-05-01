662ca39b703ba55f74fe096f378b436f96c4f5f9
Saadh Ahmed <44828093+saadhahmed00@users.noreply.github.com>
2020-05-01 16:44:22 -0400

    final sub

--- partThree.c
+++ partThree.c
@@ -4,26 +4,17 @@ int main(){
 	int input[] = {3,2,5,4,1};
 	int result;
 	int index = 0;
-	int tempmax = input[0];
+	int tempmin = input[0];
 
 	int size = sizeof(input)/sizeof(input[1]);
 
-	if(input[size-1] % 2 == 1){
-		for(index = 0; index < size; index++){
-			if(input[index] > tempmax){
-				tempmax = input[index];
-			}
-		}
-	}
-	else{
-		for(index = 0; index < size; index++){
-			if(input[index] < tempmax){
-				tempmax = input[index];
-			}
+	for(index = 0; index < size; index++){
+		if(input[index] < tempmin){
+			tempmin = input[index];
 		}
 	}
 
-	result = tempmax; 
+	result = tempmin; 
 	printf("%d \n", result);
 	return 0;
 }
--- partTwo.c
+++ partTwo.c
@@ -3,33 +3,18 @@
 int main(){
 	int input;
 	int result = 0;
-	int lastDig;
 
 	scanf("%d", &input);
 
-	lastDig = input % 10;
-
-	if(lastDig % 2 == 1){
-		while(input){
-			if(input & 1){
-				result++;
-			}
-			input >>= 1;
-		}			
-	}
-
-	else{
-		while(input){
-			if(!(input & 1)){
-				result++;
-			}
-			input >>= 1;
+	while(input){
+		if(!(input & 1)){
+			result++;
 		}
-
+		input >>= 1;
 	}
+
 
 	printf("result: %d \n", result);
 
-
-
-}
+	return 0;
+}	
